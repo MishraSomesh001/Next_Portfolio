8341126e66613d364f4710762f7cfaf4ec344206
MishraSomesh001 <mishrasomesh0403@gmail.com>
2024-12-26 23:59:05 +0530

    chore: initial commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-red-500">Somesh Mishra</div>
+    </div>
+  );
+}
